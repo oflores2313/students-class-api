4a411d5b381c0a58dfddc56121cbf9c85661cbab
Oscar Flores <oflores2313@gmail.com>
2019-10-18 18:47:10 -0500

    Added to allow for localhost post 4200

--- routes/students-class.js
+++ routes/students-class.js
@@ -2,8 +2,15 @@ const studentsClassRoutes = (app, fs) => {
 
     //variables
     const dataPath = 'data/students-class.json';
+    app.use(function(req, res, next){
+        res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+        res.header("Access-Control-Allow-Headers", "Origin , X-Requested-With, Content-Type, Accept");
 
-    //read
+        next();
+    })
+
+    //reads the json file provided in the data folder
+    //will throw an error if it cannot read it
     app.get('/students', (req, res) => {
         fs.readFile(dataPath, 'utf8', (err, data) => {
             if(err){
